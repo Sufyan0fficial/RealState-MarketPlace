28973da9f29555839a99bebd8eef29ba897cab85
Sufyan0fficial <msufyan0fficial.co@gmail.com>
2025-09-22 21:20:54 +0500

    established connection to Database via mongoose

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const DBConnection = require("./db/connect");
+dotenv.config();
+const connectionString = process.env.MONGODB_CONNECTION_STR;
 
 
 
 
-app.listen(3000,()=>{
-    console.log('Server has been started at port 3000')
-})
+const AppStarter = async () => {
+  try {
+    await DBConnection(connectionString);
+    console.log('DB connection successfully established')
+    app.listen(3000,()=>{
+        console.log('app started at port 3000')
+    })
+  } catch (error) {
+    console.log('there caught an error while starting the app',error)
+  }
+};
 
+AppStarter()
