12cd7c431a373457822075e357021a05ce830e60
Sufyan0fficial <msufyan0fficial.co@gmail.com>
2025-09-22 22:11:37 +0500

    Created user model

---
+++ Models/user.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const Schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Model = mongoose.model("User", Schema);
