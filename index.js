eedf7d434901249a7176e24e3c5e2e052af00d4f
Sufyan0fficial <msufyan0fficial.co@gmail.com>
2025-10-06 21:29:24 +0500

    replaced dev urls with production ready urls

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const { pathToFileURL } = require("url");
 uploadsCleanup()
 
 app.use(cors({
-  origin:'http://localhost:5173',
+  origin:'https://realstate-marketplace.onrender.com/',
   credentials:true
 }))
 app.use(express.json())
